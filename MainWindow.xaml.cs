0e4314e86b37eb7d090e3c9d605d2ee72446e8a3
Shuichi.Kawahara <uv8ks1227@outlook.jp>
2024-12-26 00:38:30 +0900

    プロジェクト ファイルを追加します。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System.Transactions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+using Windows.Storage.Streams;
+using System.Collections.ObjectModel;
+
+namespace BluetoothDevicesChecker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            BluetoothBatteryService batteryService = new BluetoothBatteryService();
+            var bluetoothDeviceInfoArrayTask = batteryService.GetBluetoothDeviceInformation();
+            bluetoothDeviceInfoArrayTask.ContinueWith(task =>
+            {
+                var bluetoothDeviceInfoArray = task.Result;
+                Dispatcher.Invoke(() => InitializeButtonPanel(bluetoothDeviceInfoArray));
+                Dispatcher.Invoke(() => InitializeBluetoothDeiceInfoPanel(bluetoothDeviceInfoArray));
+            });
+        }
+
+        private void InitializeButtonPanel(BluetoothDeviceInfo[] bluetoothDeviceInfo)
+        {
+            for (int i = 0; i < bluetoothDeviceInfo.Length; i++)
+            {
+                Button button = new Button
+                {
+                    Width = 100,
+                    Content = $"{bluetoothDeviceInfo[i].DeviceName}",
+                    Margin = new Thickness(0)
+                };
+
+                ButtonPanel.Children.Add(button);
+            }
+        }
+
+        private void InitializeBluetoothDeiceInfoPanel(BluetoothDeviceInfo[] bluetoothDeviceInfo) 
+        {
+            for (int i = 0; i < bluetoothDeviceInfo.Length; i++)
+            {
+                TextBlock textBlock = new TextBlock
+                {
+                    Text = $"Host Name: {bluetoothDeviceInfo[i].HostName}",
+                    Margin = new Thickness(0),
+                    FontSize = 24
+                };
+
+                BluetoothDeiceInfoPanel.Children.Add(textBlock);
+            }
+        }
+    }
+}
---
+++ Src/GetBluetoothDeviceInformation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+using Windows.Foundation;
+using Windows.Networking;
+
+namespace BluetoothDevicesChecker
+{
+    //Bluetoothデバイスの情報を保存する構造体
+    public struct BluetoothDeviceInfo
+    {
+        public string DeviceName;
+        public string HostName;
+    }
+
+    // Get Bluetooth Device HostName
+    public class BluetoothBatteryService
+    {
+        public async Task<BluetoothDeviceInfo[]> GetBluetoothDeviceInformation()
+        {
+            List<BluetoothDeviceInfo> deviceInfoList = new List<BluetoothDeviceInfo>();
+
+            // Bluetoothデバイスのセレクタを取得
+            string selector = BluetoothDevice.GetDeviceSelector();
+
+            // Bluetoothデバイスの情報を取得
+            DeviceInformationCollection devices = await DeviceInformation.FindAllAsync(selector);
+
+            foreach (var device in devices)
+            {
+                Debug.WriteLine($"Device Id: {device.Id}, Name: {device.Name}");
+                deviceInfoList.Add(new BluetoothDeviceInfo
+                {
+                    DeviceName = device.Name,
+                    HostName = device.Id
+                });
+            }
+
+            return deviceInfoList.ToArray();
+        }
+    }
+}
